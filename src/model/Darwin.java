e901a816308cc00edaddcb254c0be2b43af6b692
Eduardo <edumadrim@gmail.com>
2020-08-10 22:48:35 -0600

    add walker to darwin

--- src/model/Darwin.java
+++ src/model/Darwin.java
@@ -10,21 +10,14 @@ public class Darwin implements IConstants{
 	private HashMap<Integer, ArrayList<Robot>> generations;
 	private int genCounter;
 	private Random random;
+	private Walker walker;
 
 	public Darwin() {
 		this.generations = new HashMap<Integer, ArrayList<Robot>>();
 		this.populateFirstGen();
 		this.random = new Random();
+		this.walker = new Walker();
 		
-		// FOR GUI TEST ONLY
-		this.genCounter++;
-		ArrayList<Robot> gen = new ArrayList<Robot>();
-		
-		for (int i = 0; i < POPULATION_SIZE; i++) {
-			gen.add(new Robot(genCounter, i));
-		}
-		
-		this.generations.put(genCounter, gen);
 	}
 	
 	public Robot getIndividual(int pGen, int pNum) {
@@ -71,8 +64,7 @@ public class Darwin implements IConstants{
 		double currFit;
 		double fitnessSum = 0.0;
 		for (Robot robot : generation) {
-			// Robots are tested in the map. Their time in the map is set here
-			//Walker.walk(robot)
+			walker.walk(robot);
 			currFit = this.evaluateFitness(robot);
 			robot.setFitness(currFit);
 			fitnessSum += currFit;
